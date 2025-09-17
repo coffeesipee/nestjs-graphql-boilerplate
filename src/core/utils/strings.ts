bd041e9486040c923e7e214bb55e3279e6e2dd3f
Taufik Rahadi <taufikrahadi1@gmail.com>
2025-09-17 15:44:57 +0700

    chore: utils function to generate random string

--- src/core/utils/strings.ts
+++ src/core/utils/strings.ts
@@ -1,3 +1,9 @@
+import { randomBytes } from "crypto"
+
 export const toSlug = (str: string) => {
     return str.toLowerCase().replace(/[^a-z0-9]/g, '-').replace(/-{2,}/g, '-').replace(/^-+|-+$/g, '')
+}
+
+export const randomString = (length: number) => {
+    return randomBytes(length).toString('hex')
 }
